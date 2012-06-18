f4b6edb1a08807ff49a70afb1a48bd330ce7689d
unknown <khanrith.tor@D132048J.tower.lan>
2012-06-18 17:01:21 +1000

    update file reference

--- lib/wombat/test.rb
+++ lib/wombat/test.rb
@@ -6,9 +6,11 @@ class MyCrawler
 
   base_url "http://ipp2.bland/timesaver.aspx"
   
-  some_data "css=div.formLabelLeft", :list
-  another_info "xpath=//my/xpath[@style='selector']"
+  #some_data "css=div.formLabelLeft", :list
+  some_data "css=div#ctl00_ContentPlaceHolder1_repeaterQuestion_ctl00_theQuestion_optionList_panelQuestion .. div", :list
+  
+  #another_info "xpath=//my/xpath[@style='selector']"
 end
 
 my_cool_crawler = MyCrawler.new
-my_cool_crawler.crawl
+puts my_cool_crawler.crawl
